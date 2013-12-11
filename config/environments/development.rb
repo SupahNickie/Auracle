e899d668c40028e66149731af4f1582f9583371d
SupahNickie <nicholascase@live.com>
2013-12-11 12:16:41 -0800

    Getting ready for paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,6 @@ Minstrel::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  Paperclip.options[:command_path] = "/usr/bin/convert"
 end
