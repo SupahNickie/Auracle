d1fc1fdb350fbf10f6183799e924f38e49e078d4
SupahNickie <nicholascase@live.com>
2014-01-15 09:00:24 -0800

    Finished login tests

--- test/features/login_test.rb
+++ test/features/login_test.rb
@@ -11,6 +11,17 @@ feature "signing up a new user" do
     click_on "Sign up"
     page.text.must_include "You have signed up successfully."
   end
+
+  scenario "new bands are signed up properly" do
+    visit new_user_registration_path
+    fill_in "Username", with: "Awesome Band"
+    fill_in "Email", with: "awesomeband@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    choose "user_role_band"
+    click_on "Sign up"
+    page.text.must_include "You have signed up successfully."
+  end
 end
 
 feature "signing in a registered user" do
@@ -26,6 +37,24 @@ feature "signing in a registered user" do
     enter_band_credentials
     click_on "Sign in"
     page.text.must_include "Signed in successfully."
+  end
+end
+
+feature "signing in/up an invalid user" do
+  scenario "the login doesn't work" do
+    visit new_user_session_path
+    fill_in "Email", with: "asdfoin@asdf.com"
+    fill_in "Password", with: "qweproiu"
+    click_on "Sign in"
+    page.text.must_include "Invalid email or password."
+  end
+
+  scenario "the signup doesn't work" do
+    visit new_user_registration_path
+    fill_in "Username", with: ""
+    fill_in "Password", with: ""
+    click_on "Sign up"
+    page.text.must_include "errors prohibited this user from being saved:"
   end
 end
 
