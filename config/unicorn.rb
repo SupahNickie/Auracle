c9854680f4b262e0baa5d44b1f5eb3572ea69cb1
SupahNickie <nicholascase@live.com>
2014-04-20 21:33:29 -0700

    Increased timeout duration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
-timeout Integer(ENV['WEB_TIMEOUT'] || 15)
+timeout Integer(ENV['WEB_TIMEOUT'] || 300)
 preload_app true
 
 before_fork do |server, worker|
