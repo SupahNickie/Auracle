a05d92168688d1a0deeaa88d8f4a7c1cf6ea15b8
SupahNickie <nicholascase@live.com>
2014-07-08 23:05:05 -0700

    Fixed source problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,7 +55,7 @@ class ApplicationController < ActionController::Base
   def user_not_authorized(exception)
     policy_name = exception.policy.class.to_s.underscore
     flash[:error] = I18n.t "pundit.#{policy_name}.#{exception.query}", default: 'Sorry, you cannot perform this action.'
-    redirect_to root_path, data: {"skip-pjax" => true}
+    redirect_to root_path
   end
 
 end
